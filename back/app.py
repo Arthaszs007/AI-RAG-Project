15bf4173b113992f49fc797849f94a24e211ea81
Arthas <126498398+Arthaszs007@users.noreply.github.com>
2026-05-21 01:25:59 +0800

    Delete back/app.py

--- back/app.py
+++
@@ -1,8 +0,0 @@
-from fastapi import FastAPI
-from api.register import api_v1_router
-
-app = FastAPI(title="AI_RAG_BACKEDN")
-
-
-# register main api router
-app.include_router(api_v1_router, prefix="/api")
